c5adcf6fd1f8e63d3c77cebb8a4e7e3dcc02433b
Emelder Okafor <meldy@Emelders-MacBook-Air.local>
2025-05-06 13:13:35 +0100

    feat: updated the skills add and edit functions

--- app/models/Experience.js
+++ app/models/Experience.js
@@ -1,51 +1,51 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
 
-import sequelize from '../services/db.js';
-import JobSeekerDetail from './JobSeekerDetails.js';
+import sequelize from "../services/db.js";
+import JobSeekerDetail from "./JobSeekerDetails.js";
 
 const Experience = sequelize.define(
- 'Experience',
- {
-  id: {
-   type: DataTypes.INTEGER,
-   primaryKey: true,
-   defaultValue: () => uuidv4(),
-  },
-  jobSeekerDetailId: {
-   type: DataTypes.UUID,
-   allowNull: false,
-   references: { model: JobSeekerDetail, key: 'id' },
-  },
-  jobTitle: {
-   type: DataTypes.STRING(255),
-   allowNull: false,
-  },
-  company: {
-   type: DataTypes.STRING(255),
-   allowNull: false,
-  },
+  "Experience",
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: () => uuidv4(),
+    },
+    jobSeekerDetailId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: { model: JobSeekerDetail, key: "id" },
+    },
+    jobTitle: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    company: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
 
-  startDate: {
-   type: DataTypes.DATEONLY,
+    startDate: {
+      type: DataTypes.DATEONLY,
+    },
+    endDate: {
+      type: DataTypes.DATEONLY,
+    },
+    responsibility: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    isCurrentlyWorkingOnRole: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  endDate: {
-   type: DataTypes.DATEONLY,
-  },
-  responsibility: {
-   type: DataTypes.TEXT,
-   allowNull: true,
-  },
-  isCurrentlyWorkingOnRole: {
-   type: DataTypes.BOOLEAN,
-   defaultValue: false,
-  },
- },
- {
-  tableName: 'Experience',
-  timestamps: false,
- }
+  {
+    tableName: "Experience",
+    timestamps: false,
+  }
 );
 
 export default Experience;
